f6c4f32eba8a5e8a7fc4fc6bb0f7077a2d959d9d
Sushant Singh <70534791+vshowboy8@users.noreply.github.com>
2024-08-18 09:47:49 +0000

    changes

---
+++ src/Resume.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+function Resume() {
+  return (
+    <>
+      <div className="full-container">
+        <div className="about_container container-1440"></div>
+      </div>
+    </>
+  );
+}
+
+export default Resume;
